ad3ce17702a5f69d6bfb84aa636a14b077d4b52a
rogeriosenac <roge.pupo.toledo@gmail.com>
2024-10-04 15:56:43 -0300

    refacture script basedade

--- bkp_DB/Script_PresbiterianaJacupiranga.sql
+++ bkp_DB/Script_PresbiterianaJacupiranga.sql
@@ -40,6 +40,12 @@ CREATE TABLE depoimentos (
 
 select * from depoimentos;
 
+CREATE TABLE galeria (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    filename varchar(255) NOT NULL,
+    mime_type varchar(50) NOT NULL,
+    upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-
+select * from galeria;
 
